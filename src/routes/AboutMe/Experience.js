46f1d0e2f6d0a334ebc7734b6ebfbe149bf62be5
rajasekhar02 <rajavishnu123@gmail.com>
2022-06-02 20:32:30 -0500

    feat: formatted the eruvaka experience

--- src/routes/AboutMe/Experience.js
+++ src/routes/AboutMe/Experience.js
@@ -17,7 +17,11 @@ export default function Experience() {
             <small>Jan-2019 to Jan-2022</small>
           </div>
           <p className="mb-1">Software Engineer</p>
-          {/* <small>And some small print.</small> */}
+          <small>
+            element-ui, Highcharts, Google Maps Api, date-fns,
+            momentjs,excel.js, fuse.js, webpack, and vue.js ecosystem(vuex, vue
+            router, vuei18n, virtual scroller, vue-grid-layout).
+          </small>
         </a>
         <div
           className={`collapse ${
@@ -25,34 +29,45 @@ export default function Experience() {
           }  collapse-sftwr-dev-ervk`}
         >
           <div className="card card-body">
-            I joined the company as an Associate engineer. In the initial days I
-            started working on version 1 of Pondlogs which is a tool used for
-            configuring IOT devices manufactured by our company. I have to use
-            jquery, php and Mysql for maintaining the application along with two
-            other persons. Later I was assigned to learn the vue.js framework
-            which is used as frontend for version 2 of Pondlogs application. I
-            am the only person in the frontend for the newer version which
-            helped me in developing the ability to work independently upto one
-            year. As I was re-writing each part of the old version I learned
-            element-ui, Highcharts (library for data visualization), Google Maps
-            Api, date-fns, momentjs,excel.js, fuse.js(elastic search at
-            frontend), webpack(bundling), and vue.js ecosystem(vuex, vue router,
-            vuei18n, virtual scroller, vue-grid-layout). I have implemented
-            features as follows access control management over entire
-            application, image viewer similar to google photos using virtual
-            scroller, vue-grid-layout, fuzzy search over the data in the tables,
-            extending the functionality of components in the element-ui
-            framework to our business needs, developed a small tool for web
-            scraping using puppeteer, form builder component used in admin scope
-            of the pondlogs version, Admin Application which is used internally
-            in out company for device, customer management, Conditionally
-            toggling the features based on the user access permissions,
-            possession of devices types, Changing the fields according to the
-            user configured preferences I have learned and implemented virtual
-            row rendering, lazy loading, using web workers for heavy data
-            computation which improves the performance of the web application. I
-            learned to implement complex tasks by dividing them into small
-            manageable tasks.
+            <p>
+              I joined the company as an Associate engineer. In the initial days
+              I started working on version 1 of Pondlogs which is a tool used
+              for configuring IOT devices manufactured by our company. I have to
+              use jquery, php and Mysql for maintaining the application along
+              with two other persons.
+            </p>
+            <p>
+              Later I was assigned to learn the vue.js framework which is used
+              as frontend for version 2 of Pondlogs application. I am the only
+              person in the frontend for the newer version which helped me in
+              developing the ability to work independently upto one year. After
+              that team size increased to 3 persons. As I was re-writing each
+              part of the old version I learned element-ui, Highcharts (library
+              for data visualization), Google Maps Api, date-fns,
+              momentjs,excel.js, fuse.js(elastic search at frontend),
+              webpack(bundling), and vue.js ecosystem(vuex, vue router, vuei18n,
+              virtual scroller, vue-grid-layout).
+            </p>
+            <p>
+              I have implemented features as follows access control management
+              over entire application, image viewer similar to google photos
+              using virtual scroller, vue-grid-layout, fuzzy search over the
+              data in the tables, extending the functionality of components in
+              the element-ui framework to our business needs, developed a small
+              tool for web scraping using puppeteer, form builder component used
+              in admin scope of the pondlogs version, Admin Application which is
+              used internally in out company for device, customer management,
+              Conditionally toggling the features based on the user access
+              permissions, possession of devices types, Changing the fields
+              according to the user configured preferences.
+            </p>
+            <p>
+              I have learned and implemented virtual row rendering, lazy
+              loading, using web workers for heavy data computation which
+              improves the performance of the web application. I learned to
+              implement complex tasks by dividing them into small manageable
+              tasks.
+            </p>
           </div>
         </div>
         <a
