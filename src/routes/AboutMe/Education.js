5088d8870ac9a41e07af42bd41a75cc88432ea96
rajasekhar02 <rajavishnu123@gmail.com>
2022-06-18 22:03:16 -0500

    feat: add placeholders for education and experience details

--- src/routes/AboutMe/Education.js
+++ src/routes/AboutMe/Education.js
@@ -5,8 +5,121 @@ import get from "lodash.get";
 import { format as formatDate } from "date-fns";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
+const renderEducationDetailsCollapses = function (
+  educationDetails,
+  stringSlug
+) {
+  return educationDetails.map((eachEducationDetail) => {
+    const getEduKeyValue = (key, defaultValue = undefined) =>
+      get(eachEducationDetail, key, defaultValue);
+    return (
+      <div className="list-group" key={getEduKeyValue("educationSlug")}>
+        <a
+          href={`/about-me/${get(eachEducationDetail, "educationSlug")}`}
+          role="button"
+          data-toggle="collapse-mst-study"
+          className="list-group-item list-group-item-action"
+          aria-current="true"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">
+              {get(eachEducationDetail, "universityName")}
+            </h5>
+            <small
+              className={`${
+                get(eachEducationDetail, "isCurrent", false) && "fw-bolder fs-6"
+              }`}
+            >
+              {`${formatDate(
+                new Date(get(eachEducationDetail, "startDate", new Date())),
+                "MMM yyyy"
+              )}
+                    -
+                    ${formatDate(
+                      new Date(get(eachEducationDetail, "endDate", new Date())),
+                      "MMM yyyy"
+                    )}
+                  `}
+            </small>
+          </div>
+          <p className="mb-1">{get(eachEducationDetail, "specialization")}</p>
+        </a>
+        <div
+          className={`collapse ${
+            [get(eachEducationDetail, "educationSlug"), undefined].includes(
+              stringSlug
+            ) &&
+            get(eachEducationDetail, "coursesEnrolledCollection.items", [])
+              .length !== 0 &&
+            "show"
+          }`}
+        >
+          <div className="card card-body">
+            {get(
+              eachEducationDetail,
+              "coursesEnrolledCollection.items",
+              []
+            ).map((eachCourse) => {
+              return (
+                <details key={eachCourse.title}>
+                  <summary>{eachCourse.title}</summary>
+                  {documentToReactComponents(
+                    get(eachCourse, "courseDescription.json")
+                  )}
+                </details>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  });
+};
+
+const renderEducationDetailsPlaceHolder = function () {
+  return [1, 2].map((listItem) => {
+    return (
+      <div
+        className="list-group placeholder-glow"
+        aria-hidden="true"
+        key={listItem}
+      >
+        <div
+          // href={`/about-me/${get(eachEducationDetail, "educationSlug")}`}
+          role="button"
+          data-toggle="collapse-mst-study"
+          className="list-group-item list-group-item-action"
+          aria-current="true"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <span className="placeholder col-8"></span>
+            <span className="placeholder col-2"></span>
+          </div>
+          <span className="mt-2 placeholder col-4"></span>
+        </div>
+        <div className={`collapse ${listItem === 1 && "show"}`}>
+          <div className="card card-body">
+            {[1, 2].map((placeholder) => {
+              return (
+                // <div key={placeholder}>
+                <span className="ms-n2">
+                  <span class="ms-2 placeholder col-5"></span>
+                  <span class="ms-2 placeholder col-3"></span>
+                  <span class="ms-2 placeholder col-1"></span>
+                  <span class="ms-2 placeholder col-2"></span>
+                  <span class="ms-2 placeholder col-4"></span>
+                  <span class="ms-2 placeholder col-2"></span>
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  });
+};
 export default function Education() {
-  const { string_slug } = useParams();
+  const { string_slug: stringSlug } = useParams();
   const [educationDetails, setEducationDetials] = React.useState([]);
   useEffect(() => {
     (async function () {
@@ -17,74 +130,9 @@ export default function Education() {
   return (
     <section className="education mt-3">
       <p className="heading fw-bold fs-4 border-bottom">Education</p>
-      {educationDetails.map((eachEducationDetail) => {
-        const getEduKeyValue = (key, defaultValue = undefined) =>
-          get(eachEducationDetail, key, defaultValue);
-        return (
-          <div className="list-group" key={getEduKeyValue("educationSlug")}>
-            <a
-              href={`/about-me/${get(eachEducationDetail, "educationSlug")}`}
-              role="button"
-              data-toggle="collapse-mst-study"
-              className="list-group-item list-group-item-action"
-              aria-current="true"
-            >
-              <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">
-                  {get(eachEducationDetail, "universityName")}
-                </h5>
-                <small
-                  className={`${
-                    get(eachEducationDetail, "isCurrent", false) &&
-                    "fw-bolder fs-6"
-                  }`}
-                >
-                  {`${formatDate(
-                    new Date(get(eachEducationDetail, "startDate", new Date())),
-                    "MMM yyyy"
-                  )}
-                    -
-                    ${formatDate(
-                      new Date(get(eachEducationDetail, "endDate", new Date())),
-                      "MMM yyyy"
-                    )}
-                  `}
-                </small>
-              </div>
-              <p className="mb-1">
-                {get(eachEducationDetail, "specialization")}
-              </p>
-            </a>
-            <div
-              className={`collapse ${
-                [get(eachEducationDetail, "educationSlug"), undefined].includes(
-                  string_slug
-                ) &&
-                get(eachEducationDetail, "coursesEnrolledCollection.items", [])
-                  .length !== 0 &&
-                "show"
-              }`}
-            >
-              <div className="card card-body">
-                {get(
-                  eachEducationDetail,
-                  "coursesEnrolledCollection.items",
-                  []
-                ).map((eachCourse) => {
-                  return (
-                    <details key={eachCourse.title}>
-                      <summary>{eachCourse.title}</summary>
-                      {documentToReactComponents(
-                        get(eachCourse, "courseDescription.json")
-                      )}
-                    </details>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-        );
-      })}
+      {educationDetails.length === 0
+        ? renderEducationDetailsPlaceHolder()
+        : renderEducationDetailsCollapses(educationDetails, stringSlug)}
     </section>
   );
 }
--- src/routes/AboutMe/Experience.js
+++ src/routes/AboutMe/Experience.js
@@ -22,8 +22,110 @@ const richTextToReactOptions = {
   }
 };
 
+const renderExperienceDetailsCollapse = function (
+  experienceDetails,
+  stringSlug
+) {
+  return experienceDetails.map((eachExperience) => {
+    const getExpKeyValue = (key, defaultValue = undefined) =>
+      get(eachExperience, key, defaultValue);
+    return (
+      <div className="list-group" key={getExpKeyValue("experienceSlug")}>
+        <a
+          href={`/about-me/${getExpKeyValue("experienceSlug")}`}
+          role="button"
+          data-toggle="collapse-mst-study"
+          className="list-group-item list-group-item-action"
+          aria-current="true"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">{getExpKeyValue("companyName")}</h5>
+            <small>
+              {`${
+                getExpKeyValue("isCurrent", false) ? "Current" : ""
+              } ${formatDate(
+                new Date(getExpKeyValue("startDate", new Date())),
+                "MMM yyyy"
+              )}
+                    -
+                    ${formatDate(
+                      new Date(getExpKeyValue("endDate", new Date())),
+                      "MMM yyyy"
+                    )}
+                  `}
+            </small>
+          </div>
+          <p className="mb-1">{getExpKeyValue("role")}</p>
+          <small className="text-muted">
+            {getExpKeyValue("technologiesUsed")}
+          </small>
+        </a>
+        <div
+          className={`collapse ${
+            [getExpKeyValue("experienceSlug"), undefined].includes(
+              stringSlug
+            ) && "show"
+          }`}
+        >
+          <div className="card card-body">
+            {documentToReactComponents(
+              getExpKeyValue("jobDescription.json"),
+              richTextToReactOptions
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  });
+};
+
+const renderExperienceDetailsPlaceHolder = function () {
+  return [1, 2].map((listItem) => {
+    return (
+      <div
+        className="list-group placeholder-glow"
+        aria-hidden="true"
+        key={listItem}
+      >
+        <div
+          // href={`/about-me/${get(eachEducationDetail, "educationSlug")}`}
+          role="button"
+          data-toggle="collapse-mst-study"
+          className="list-group-item list-group-item-action"
+          aria-current="true"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <span className="placeholder col-8"></span>
+            <span className="placeholder col-2"></span>
+          </div>
+          <div className="mt-2 placeholder col-4"></div>
+          {/* <br /> */}
+          <div className="mt-2 placeholder col-9"></div>
+        </div>
+        <div className={`collapse show`}>
+          <div className="card card-body">
+            {[1, 2].map((placeholder) => {
+              return (
+                // <div key={placeholder}>
+                <span className="ms-n2">
+                  <span class="ms-2 placeholder col-5"></span>
+                  <span class="ms-2 placeholder col-3"></span>
+                  <span class="ms-2 placeholder col-1"></span>
+                  <span class="ms-2 placeholder col-2"></span>
+                  <span class="ms-2 placeholder col-4"></span>
+                  <span class="ms-2 placeholder col-2"></span>
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  });
+};
+
 export default function Experience() {
-  const { string_slug } = useParams();
+  const { string_slug: stringSlug } = useParams();
   const [experienceDetails, setExperienceDetials] = React.useState([]);
   useEffect(() => {
     (async function () {
@@ -34,57 +136,9 @@ export default function Experience() {
   return (
     <section className="experience mt-3">
       <p className="heading fw-bold fs-4 border-bottom">Experience</p>
-      {experienceDetails.map((eachExperience) => {
-        const getExpKeyValue = (key, defaultValue = undefined) =>
-          get(eachExperience, key, defaultValue);
-        return (
-          <div className="list-group" key={getExpKeyValue("experienceSlug")}>
-            <a
-              href={`/about-me/${getExpKeyValue("experienceSlug")}`}
-              role="button"
-              data-toggle="collapse-mst-study"
-              className="list-group-item list-group-item-action"
-              aria-current="true"
-            >
-              <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">{getExpKeyValue("companyName")}</h5>
-                <small>
-                  {`${
-                    getExpKeyValue("isCurrent", false) ? "Current" : ""
-                  } ${formatDate(
-                    new Date(getExpKeyValue("startDate", new Date())),
-                    "MMM yyyy"
-                  )}
-                    -
-                    ${formatDate(
-                      new Date(getExpKeyValue("endDate", new Date())),
-                      "MMM yyyy"
-                    )}
-                  `}
-                </small>
-              </div>
-              <p className="mb-1">{getExpKeyValue("role")}</p>
-              <small className="text-muted">
-                {getExpKeyValue("technologiesUsed")}
-              </small>
-            </a>
-            <div
-              className={`collapse ${
-                [getExpKeyValue("experienceSlug"), undefined].includes(
-                  string_slug
-                ) && "show"
-              }`}
-            >
-              <div className="card card-body">
-                {documentToReactComponents(
-                  getExpKeyValue("jobDescription.json"),
-                  richTextToReactOptions
-                )}
-              </div>
-            </div>
-          </div>
-        );
-      })}
+      {experienceDetails.length === 0
+        ? renderExperienceDetailsPlaceHolder()
+        : renderExperienceDetailsCollapse(experienceDetails, stringSlug)}
     </section>
   );
 }
