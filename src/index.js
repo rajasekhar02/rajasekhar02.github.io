bab26827adaf3b743064221ab2f11146a4b49e81
rajasekhar02 <raja02sekhar08@gmail.com>
2023-09-11 15:33:15 -0500

    refactor: removed splitwise api implementation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import App from "./App";
-import AboutMe from "./routes/AboutMe/index.js";
-import LearningReferences from "./routes/LearningReferences";
-import Projects from "./routes/Projects/index.js";
-import ProjectGrids from "./routes/Projects/ProjectGrids";
-import Splitwise from "./routes/Projects/Splitwise";
-import RedirectHandler from "./routes/Projects/Splitwise/RedirectHandler";
-import AuthHandler from "./routes/Projects/AuthHandler";
-import { AuthProvider } from "./routes/Projects/AuthContext";
-import Dashboard from "./routes/Projects/Splitwise/Dashboard";
-const rootElement = document.getElementById("root");
+import App from './App';
+import AboutMe from './routes/AboutMe/index.js';
+import LearningReferences from './routes/LearningReferences';
+import Projects from './routes/Projects/index.js';
+import ProjectGrids from './routes/Projects/ProjectGrids';
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
   // <StrictMode>
@@ -23,26 +18,8 @@ root.render(
         <Route path="about-me" element={<AboutMe />}>
           <Route path=":string_slug" element={<AboutMe />} />
         </Route>
-        <Route
-          path="projects"
-          element={
-            <AuthProvider>
-              <Projects />
-            </AuthProvider>
-          }
-        >
+        <Route path="projects" element={<Projects />}>
           <Route index element={<ProjectGrids />}></Route>
-          <Route path="splitwise" element={<Splitwise />}>
-            <Route path="redirect" element={<RedirectHandler />}></Route>
-            <Route
-              path="dashboard"
-              element={
-                <AuthHandler>
-                  <Dashboard />
-                </AuthHandler>
-              }
-            ></Route>
-          </Route>
         </Route>
         <Route
           path="learning-references"
