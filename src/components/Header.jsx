95192178b93248f48beeacf87303b3e990e4f89e
raja sekhar <raja02sekhar08@gmail.com>
2023-10-04 15:50:01 -0500

    feat: importing the svg files using vite svgr plugin. But tailwind classes are not working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
-import ProfilePicture from "../routes/AboutMe/ProfilePicture";
+// import ProfilePicture from "../routes/AboutMe/ProfilePicture";
+import ThemeDarkIcon from "src/assets/theme-dark-icon.svg?react";
+import ThemeLightIcon from "src/assets/theme-light-icon.svg?react";
 
 export default function Header() {
   return (
@@ -102,46 +104,23 @@ export default function Header() {
                     ></div>
                     <nav className="pointer-events-auto hidden md:block">
                       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-                        <li>
-                          <a
-                            className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                            href="/about"
-                          >
-                            About
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                            href="/articles"
-                          >
-                            Articles
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                            href="/projects"
-                          >
-                            Projects
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                            href="/speaking"
-                          >
-                            Speaking
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                            href="/uses"
-                          >
-                            Uses
-                          </a>
-                        </li>
+                        {[
+                          ["About", "/about"],
+                          ["Articles", "/articles"],
+                          ["Projects", "/projects"],
+                          ["Uses", "/uses"]
+                        ].map((eachMenuItem, index) => {
+                          return (
+                            <li key={`menu-item-${index}`}>
+                              <a
+                                className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
+                                href={eachMenuItem[1]}
+                              >
+                                {eachMenuItem[0]}
+                              </a>
+                            </li>
+                          );
+                        })}
                       </ul>
                     </nav>
                   </div>
@@ -149,35 +128,11 @@ export default function Header() {
                     <div className="pointer-events-auto">
                       <button
                         type="button"
-                        aria-label="Switch to dark theme"
+                        aria-label="Switch to light theme"
                         className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
                       >
-                        <svg
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          aria-hidden="true"
-                          className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600"
-                        >
-                          <path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z"></path>
-                          <path
-                            d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
-                            fill="none"
-                          ></path>
-                        </svg>
-                        <svg
-                          viewBox="0 0 24 24"
-                          aria-hidden="true"
-                          className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500"
-                        >
-                          <path
-                            d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          ></path>
-                        </svg>
+                        <ThemeDarkIcon />
+                        <ThemeLightIcon />
                       </button>
                     </div>
                   </div>
