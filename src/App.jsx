aa67e750b00fb5b384752fa8d2a034e7a21587b1
raja sekhar <raja02sekhar08@gmail.com>
2023-10-04 16:56:20 -0500

    feat: add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./style.scss";
 import { Outlet } from "react-router-dom";
-import NavBar from "./components/NavBar";
 import BackGroundLayer from "./components/BackGroundLayer";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
