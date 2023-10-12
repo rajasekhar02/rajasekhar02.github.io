64d8977cf0d42d14c594249037f3e68c02ca9852
raja sekhar <raja02sekhar08@gmail.com>
2023-10-12 14:27:47 -0500

    feat: add icon

--- src/components/Header/ProfilePicture.jsx
+++ src/components/Header/ProfilePicture.jsx
@@ -1,4 +1,5 @@
 import logo from "src/assets/profile-picture.png"; // Tell webpack this JS file uses this image
+import ProfilePicPlaceholder from "src/assets/placeholder.svg?react";
 import { useAboutMe } from "../../routes/AboutMe/AboutMeContext";
 import get from "lodash.get";
 export default function ProfilePicture() {
@@ -17,19 +18,7 @@ export default function ProfilePicture() {
           alt="profile picture"
         />
       ) : (
-        <svg
-          className="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800 h-16 w-16"
-          width="150"
-          height="180"
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-label="Placeholder"
-          preserveAspectRatio="xMidYMid slice"
-          focusable="false"
-        >
-          <title>Placeholder</title>
-          <rect width="100%" height="100%" fill="#868e96"></rect>
-        </svg>
+        <ProfilePicPlaceholder></ProfilePicPlaceholder>
       )}
     </a>
   );
