56eb90fbaf69076a7effcbf8142d6874d86b4b1a
raja sekhar <raja02sekhar08@gmail.com>
2023-10-23 16:32:53 -0500

    feat: add changes

--- src/routes/Readings/Home.jsx
+++ src/routes/Readings/Home.jsx
@@ -17,12 +17,14 @@ export default function Home() {
       <div className="mx-auto max-w-2xl lg:max-w-5xl">
         <header className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Readings on Frontend, System Design, Databases to keep my-self
-            updated
+            My Weekly Learning Nuggets
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            All of my long-form thoughts on programming, leadership, product
-            design, and more, collected in chronological order.
+            {` Welcome to my weekly learning review! Here's a look at what I've
+            been reading and watching lately, and what I've learned. I'm always
+            on the lookout for new and interesting things to learn, so I'm
+            excited to share this week's picks with you. I hope you find
+            something that interests you, too!`}
           </p>
         </header>
         <div className="mt-16 sm:mt-20">
